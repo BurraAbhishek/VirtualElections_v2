e0ef00e419e071178aec0fe18c5bd6fac1fb7a32
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2022-01-25 23:48:07 +0530

    provision: Add more election controls

--- bin/mongoinserts/add_election_controls.py
+++ bin/mongoinserts/add_election_controls.py
@@ -12,7 +12,29 @@ access_controls = {
 candidate_privacy_restrictions = {
     "_id": "candidate_privacy",
     "views": "Default",
-    "showTosViolation": "True"
+    "showTosViolation": True
+}
+
+
+candidate_vote = {
+    "_id": "candidate_must_vote",
+    "boolValue": False
+}
+
+
+voter_age_constraints = {
+    "_id": "voter_ages",
+    "boolRequired": False,
+    "minAge": 0,
+    "maxAge": 100,
+}
+
+
+candidate_age_constraints = {
+    "_id": "candidate_ages",
+    "boolRequired": False,
+    "minAge": 0,
+    "maxAge": 100,
 }
 
 
@@ -24,4 +46,16 @@ def add_access_controls():
     mongo_client.db_insert_one(
         collection="mod2",
         data=candidate_privacy_restrictions
+    )
+    mongo_client.db_insert_one(
+        collection="mod2",
+        data=voter_age_constraints
+    )
+    mongo_client.db_insert_one(
+        collection="mod2",
+        data=candidate_age_constraints
+    )
+    mongo_client.db_insert_one(
+        collection="mod2",
+        data=candidate_vote
     )
