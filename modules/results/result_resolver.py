778aba69c62f8531d2587b54fd0dd6965ad9697d
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2022-02-02 22:35:09 +0530

    provision: Improved efficiency and insights
    Calculating the results for a large number of votes takes time. Every time the results are viewed, this computation takes place, slowing down the response time. Instead, store the results separately in a database and call them whenever required.
    This commit also creates a provision for voter turnout by gender as an insight.

--- modules/results/result_resolver.py
+++ modules/results/result_resolver.py
@@ -1,9 +1,46 @@
-from itertools import count
 from bin.mongodb import mongo_client
 from modules.common import crypt
 
 
-def resolve_result():
+def gender_turnout():
+    turnout = mongo_client.db_get_collection("turnout")
+    vote5 = mongo_client.db_get_collection("vote5").find()
+    user_vote5 = []
+    gender_composition = {
+        "_id": "gender",
+        "Male": 0,
+        "Female": 0,
+        "Other": 0,
+        "Total": 0
+    }
+    for i in vote5:
+        user_vote5.append(crypt.str_decrypt(i["_id"]))
+    user4 = mongo_client.db_get_collection("user4").find()
+    for i in user4:
+        if i["_id"] in user_vote5:
+            gender_composition[i["gender"]] += 1
+            gender_composition["Total"] += 1
+    try:
+        turnout.insert_one(gender_composition)
+    except:
+        turnout.update_one(
+            {
+                '_id': "gender"
+            },
+            {
+                "$set": {
+                    'Male': gender_composition["Male"],
+                    'Female': gender_composition["Female"],
+                    'Other': gender_composition["Other"],
+                    'Total': gender_composition["Total"]
+                }
+            }
+        )
+
+
+def recompute_result():
+    user5 = mongo_client.db_get_collection("user5")
+    user5.drop()
     vote5 = list(mongo_client.db_get_collection("vote5").find())
     user3 = mongo_client.db_get_collection("user3")
     candidates_with_votes = []
@@ -13,14 +50,25 @@ def resolve_result():
             candidates_with_votes.append(i["value"])
             candidate = user3.find_one({"_id": crypt.str_decrypt(i["value"])})
             result_list.append({
-                "vote_id": i["value"],
-                "candidate": candidate["party_name"],
+                "_id": i["value"],
+                "candidate": crypt.str_encrypt(candidate["party_name"]),
                 "count": 1
             })
         else:
             for j in result_list:
-                if j["vote_id"] == i["value"]:
+                if j["_id"] == i["value"]:
                     j["count"] += 1
     candidates_with_votes.clear()
     result_list.sort(key=lambda x: x["count"], reverse=True)
+    user5 = mongo_client.db_get_collection("user5")
+    for i in result_list:
+        user5.insert_one(i)
     return result_list
+
+
+def resolve_result():
+    user5 = mongo_client.db_get_collection("user5")
+    user5_list = list(user5.find())
+    for i in user5_list:
+        i["candidate"] = crypt.str_decrypt(i["candidate"])
+    return user5_list
--- modules/results/result_view.py
+++ modules/results/result_view.py
@@ -1,7 +1,16 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.core.exceptions import PermissionDenied
 from bin.mongodb import mongo_client
-from modules.results.result_resolver import resolve_result
+from modules.results import result_resolver
+
+
+def reset_result(request):
+    if not request.session.get("mod"):
+        raise PermissionDenied
+    result_resolver.gender_turnout()
+    result_resolver.recompute_result()
+    return HttpResponseRedirect("/modzone/authenticate")
 
 
 def result_view(request):
@@ -11,7 +20,7 @@ def result_view(request):
     )
     if not authorization["canShowResults"]:
         raise PermissionDenied
-    result_list = resolve_result()
+    result_list = result_resolver.resolve_result()
     return render(
         request,
         "results/show_result.html",
