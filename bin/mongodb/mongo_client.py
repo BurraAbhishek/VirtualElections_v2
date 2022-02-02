894b25946fdc84cbd98ecc3c9229d0a1d311965d
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2022-02-02 21:41:07 +0530

    formatting: PEP8 function names
    Reference: https://www.python.org/dev/peps/pep-0008/#function-and-variable-names. Function names are preferably lowercase with words separated by underscores as necessary.

--- bin/mongodb/mongo_client.py
+++ bin/mongodb/mongo_client.py
@@ -1,7 +1,7 @@
 import pymongo
 
 
-def getConnection():
+def get_connection():
     # Name of the database
     databaseName = "virtualelections"
 
@@ -17,7 +17,7 @@ def getConnection():
     return database
 
 
-def ifCollectionExists(database, collection) -> bool:
+def if_collection_exists(database, collection) -> bool:
     result = False
     collection_list = database.list_collection_names()
     if collection in collection_list:
@@ -26,8 +26,8 @@ def ifCollectionExists(database, collection) -> bool:
 
 
 def db_get_collection(collection):
-    database = getConnection()
-    if not ifCollectionExists(database, collection):
+    database = get_connection()
+    if not if_collection_exists(database, collection):
         database.create_collection(collection)
     db_collection = database.get_collection(collection)
     return db_collection
