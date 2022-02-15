cd90822ec28445fa36e0c4cb0aac4ea336234d44
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2022-02-15 20:05:43 +0530

    provision: Separate functions
    Having separate functions for rendering, vote casting and request processing makes debugging and addition of new features easier, in addition to more readable and understandable code.

--- modules/election/cast.py
+++ modules/election/cast.py
@@ -6,6 +6,32 @@ from modules.common.shrug import is_shrug
 from modules.common import crypt
 
 
+def successfully_voted(request) -> render:
+    return render(
+        request,
+        "oops/security_closepage.html",
+        {
+            "success_text": "Your vote was successfully cast",
+            "success_message": " ".join(
+                [
+                    "Your vote was successfully cast.",
+                    "This means that you voted successfully."
+                ]
+            )
+        }
+    )
+
+
+def process_vote(request, vote_candidate_id):
+    vote5 = mongo_client.db_get_collection("vote5")
+    voter = request.session["voter"]
+    encoded_vote = {
+        "_id": crypt.str_encrypt(voter),
+        "value": crypt.str_encrypt(vote_candidate_id),
+    }
+    vote5.insert_one(encoded_vote)
+
+
 def cast_vote(request):
     authorizationcollection = mongo_client.db_get_collection("mod2")
     authorization = authorizationcollection.find_one(
@@ -15,7 +41,6 @@ def cast_vote(request):
         raise PermissionDenied
     if request.method == "POST":
         vote = CastVote(request.POST)
-        print(vote.data)
         if vote.is_valid():
             vote_candidate_id = str(vote.cleaned_data["party_id"])
             if is_shrug(vote_candidate_id):
@@ -24,31 +49,12 @@ def cast_vote(request):
                 if user3_voted["tosViolation"] is True:
                     raise PermissionDenied
                 else:
-                    vote5 = mongo_client.db_get_collection("vote5")
-                    voter = request.session["voter"]
-                    encoded_vote = {
-                        "_id": crypt.str_encrypt(voter),
-                        "value": crypt.str_encrypt(vote_candidate_id),
-                    }
-                    vote5.insert_one(encoded_vote)
+                    process_vote(request, vote_candidate_id)
                     request.session.__delitem__("voter")
-                    return render(
-                        request,
-                        "oops/security_closepage.html",
-                        {
-                            "success_text": "Your vote was successfully cast",
-                            "success_message": " ".join(
-                                [
-                                    "Your vote was successfully cast.",
-                                    "This means that you voted successfully."
-                                ]
-                            )
-                        }
-                    )
+                    return successfully_voted(request)
             else:
                 raise PermissionDenied
         else:
-            print("X")
             raise PermissionDenied
     else:
         raise PermissionDenied
