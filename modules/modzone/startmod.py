1a5ce18bef6c09840cce7c661636bbcc61b8f3f3
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2022-02-21 22:42:36 +0530

    provision: Mark unauthorized login attempts

--- modules/modzone/startmod.py
+++ modules/modzone/startmod.py
@@ -3,6 +3,31 @@ from django.core.exceptions import PermissionDenied
 from django.contrib.auth.hashers import check_password
 from bin.mongodb import mongo_client
 from modules.modzone.forms import ModLogin
+from modules.common.shrug import is_shrug
+
+
+def log_violation(request) -> render:
+    if request.session.get("party"):
+        user3 = mongo_client.db_get_collection("user3")
+        suspect = request.session["party"]
+        if is_shrug(suspect):
+            user3.update_one(
+                {
+                    "_id": suspect
+                },
+                {
+                    "$set": {
+                        "tosViolation": True
+                    }
+                }
+            )
+    else:
+        request.session["unauthorized"] = 1
+    return render(
+        request,
+        "oops/corrections_fakemod.html",
+        status=403
+    )
 
 
 def authenticate_mod(request) -> render:
@@ -23,20 +48,11 @@ def authenticate_mod(request) -> render:
                         "modzone/mainmenu.html",
                     )
                 else:
-                    return render(
-                        request,
-                        "oops/corrections_fakemod.html",
-                    )
+                    return log_violation(request)
             except:
-                return render(
-                    request,
-                    "oops/corrections_fakemod.html",
-                )
+                return log_violation(request)
         else:
-            return render(
-                request,
-                "oops/corrections_fakemod.html",
-            )
+            return log_violation(request)
     else:
         if request.session.get("mod"):
             return render(
